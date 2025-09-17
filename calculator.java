dea260da1c9aa5a5fb62bc5293e6e1b4c27b506f
Muhammad Zeeshan Zaheer <shan6818@gmail.com>
2025-09-17 13:54:01 +0500

    Added the java program to calculate sum

---
+++ calculator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class calculator {
+  public static void main(String[] args) {
+    int a, b;
+    Scanner s = new Scanner(System.in);
+
+    System.out.print("Enter the value of a: ");
+    a =s.nextInt();
+
+    System.out.print("Enter the value of b: ");
+    b = s.nextInt();
+    s.close();
+    a = a+b;
+
+    System.out.println("The value of sum is " +a);
+
+
+  }
+}
